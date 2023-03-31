12256e7cd4e449cac5ace16e67c804cfcef4d6b3
Ulu-pro <web.ulu7@gmail.com>
2023-03-31 20:55:51 +0500

    Simplified quotes and commented sending venue

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-require "bot.php";
-require "env.php";
+require 'bot.php';
+require 'env.php';
 
-$bot = new Bot(Env::$TOKEN);
+$bot = new Bot(Env::TOKEN);
 $data = $bot->getData();
 
 if (isset($data['message'])) {
@@ -10,41 +10,51 @@ if (isset($data['message'])) {
   $id = $message['message_id'];
   checkStatus($message);
 
-  $group = $message['chat']['id'] == Env::$GROUP_ID;
-  $user = $message['from']['id'] == Env::$USER_ID;
-  $caption = $message['caption'] == Env::$CAPTION;
+  $group = $message['chat']['id'] == Env::GROUP_ID;
+  $user = $message['from']['id'] == Env::USER_ID;
+  $caption = $message['caption'] == Env::CAPTION;
 
   if ($group && $user && $caption) {
-    $message_id = $bot->sendMessage(Env::$GROUP_ID, Env::$REPLY, $id);
-    $bot->editMessage(Env::$GROUP_ID, $message_id, Env::$REPLY, json_encode([
+    $message_id = $bot->sendMessage(Env::GROUP_ID, Env::REPLY, $id);
+    $bot->editMessageText(Env::GROUP_ID, $message_id, Env::REPLY, json_encode([
         'inline_keyboard' => [
           [
             [
               'text' => getWeather(),
-              'url' => 'tg://user?id=' . Env::$ADMIN_ID
+              'url' => 'tg://user?id=' . Env::ADMIN_ID
             ]
           ]
         ]
-    ], true));
+    ], JSON_HEX_TAG));
+    /*
+    $bot->sendVenue(
+        Env::ADMIN_ID,
+        Env::LATITUDE,
+        Env::LONGITUDE,
+        Env::TITLE,
+        Env::ADDRESS,
+        $id
+    );
+    */
   }
 }
 
 function checkStatus($message) {
   global $bot;
   $id = $message['message_id'];
-  $admin = $message['from']['id'] == Env::$ADMIN_ID;
-  $command = $message['text'] == Env::$COMMAND;
+  $admin = $message['from']['id'] == Env::ADMIN_ID;
+  $command = $message['text'] == Env::COMMAND;
 
   if ($admin && $command) {
-    $bot->sendMessage(Env::$ADMIN_ID, getWeather(), $id);
+    $bot->sendMessage(Env::ADMIN_ID, getWeather(), $id);
   }
 }
 
 function getWeather() {
-  $data = json_decode(file_get_contents(Env::$URL), true)['current'];
+  $data = json_decode(file_get_contents(Env::URL), true)['current'];
 
-  $temperature = (int) $data['temp'] . Env::$CELSIUS;
-  $feels_like = (int) $data['feels_like'] . Env::$CELSIUS;
+  $temperature = (int) $data['temp'] . Env::CELSIUS;
+  $feels_like = (int) $data['feels_like'] . Env::CELSIUS;
   $description = $data['weather'][0]['description'];
 
   $description = mb_strtoupper(
